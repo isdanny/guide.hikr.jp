6c7622779a79bd6df6605fff69b755f5509aa85f
Leszek <leszek@rybicki.cc>
2013-10-19 00:02:59 +0900

    must read size correctly

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -31,7 +31,8 @@ def image_template(url, size, klass='' )
         f.puts YAML::dump(img)
     end
   end
-  "<div class='flickr-photo #{klass}'><a target='_blank' href='#{img[:url]}'><img title='#{img[:title]}' alt='#{img[:title]}' src='#{img[size]}'></a><div class='caption'>Photo by <a target='_blank' href='#{img[:owner_url]}'>#{img[:owner_name]}</a> </div></div>"
+  p img
+  "<div class='flickr-photo #{klass}'><a target='_blank' href='#{img[:url]}'><img title='#{img[:title]}' alt='#{img[:title]}' src='#{img[":#{size}"]}'></a><div class='caption'>Photo by <a target='_blank' href='#{img[:owner_url]}'>#{img[:owner_name]}</a> </div></div>"
 end
 
 module Jekyll
