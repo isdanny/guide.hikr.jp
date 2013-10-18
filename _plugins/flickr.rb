a86e2875ee0b3f78b99db061b68433c7bd6e43e0
Leszek Rybicki <leszek@rybicki.cc>
2013-10-18 23:56:19 +0900

    flickr fix

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -1,19 +1,41 @@
 require 'liquid'
 require 'fleakr'
+require 'digest'
 
 Fleakr.api_key       = "06a5a0dfe07d9d2d0af5ae6fe74bad03"
 Fleakr.shared_secret = "c211ffcb95dc9680"
 Fleakr.auth_token    = ""
 
-CACHED_IMAGES = {}
+ROOT = File.absolute_path(".")
 
-def image_template(url, size, klass='')
-  img = CACHED_IMAGES[url] ||= Fleakr.resource_from_url(url) 
-  imgfile = img.send(size||'medium')
-  "<div class='flickr-photo #{klass}'><a target='_blank' href='#{img.url}'><img title='#{img.title}' alt='#{img.title}' src='#{imgfile.url}'></a><div class='caption'>Photo by <a target='_blank' href='#{img.owner.photos_url}'>#{img.owner.name||img.owner.username}</a> </div></div>"   
+
+def image_template(url, size, klass='' )
+  hash = Digest::MD5.base64digest(url).delete!("=").gsub("/","_")
+  path = File.join(ROOT,"_cache","img-#{hash}.yml")
+  if File.exist? path 
+    img = YAML::load( File.open( path ) )
+    p "reading #{path}"
+  else
+    fimg = Fleakr.resource_from_url(url)
+    img = {
+      :url=>fimg.url,
+      :title=>fimg.title,
+      :owner_url=>fimg.owner.photos_url,
+      :owner_name=>fimg.owner.name||fimg.owner.username,
+    }
+    [:square, :thumbnail, :small, :medium, :large, :original].each do |size|
+      img[size] = fimg.send(size).url
+    end
+    p "writing #{path}"
+    File.open(path, 'w+') do |f|
+        f.puts YAML::dump(img)
+    end
+  end
+  "<div class='flickr-photo #{klass}'><a target='_blank' href='#{img[:url]}'><img title='#{img[:title]}' alt='#{img[:title]}' src='#{img[size]}'></a><div class='caption'>Photo by <a target='_blank' href='#{img[:owner_url]}'>#{img[:owner_name]}</a> </div></div>"
 end
 
 module Jekyll
+
   class FlickrTag < Liquid::Tag
 
     def initialize(tag_name, text, tokens)
@@ -25,7 +47,7 @@ module Jekyll
       params = @text.split(/\s/)
       url = params[0]
       size = params[1] || 'medium'        
-      image_template(url, size)
+      image_template(url, size )
     end
   end
 end
@@ -38,3 +60,4 @@ end
 
 Liquid::Template.register_tag('flickr', Jekyll::FlickrTag)
 Liquid::Template.register_filter(Flickr)
+
