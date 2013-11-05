743d907ef3a9107c03cc66b61e50a0a5454568b8
Leszek <leszek@rybicki.cc>
2013-11-06 00:12:02 +0900

    cleanup

--- Gemfile
+++ Gemfile
@@ -12,6 +12,5 @@ gem 'therubyracer'
 
 
 gem 'jekyll'
-gem 'rack-jekyll'
-gem 'RedCloth'
+gem 'rack-jekyll', :git => 'https://github.com/adaoraul/rack-jekyll.git', :require => 'rack/jekyll'
 gem 'thin'
--- _site/fleakr/lib/fleakr/support.rb
+++
@@ -1,7 +0,0 @@
-require 'fleakr/support/attribute'
-require 'fleakr/support/object'
-require 'fleakr/support/request'
-require 'fleakr/support/url_expander'
-require 'fleakr/support/utility'
-require 'fleakr/support/association'
-require 'fleakr/support/cache'
