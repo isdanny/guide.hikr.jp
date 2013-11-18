90c6aa3752b1e090e610333f81a6554cf83c08c4
Leszek <leszek@rybicki.cc>
2013-11-19 08:39:37 +0900

    yamakita area and autumn colors

--- _plugins/flickr.rb
+++ _plugins/flickr.rb
@@ -40,7 +40,12 @@ end
 
 def image_template(url, size, klass='' )
   img = flickr_image(url)
-  "<div class='flickr-photo #{klass}'><img title='"+img['title']+"' alt='"+img['title']+"' src='"+img[size]+"'><div class='caption'><div class='photo-date'>"+img['taken']+"</div><div class='copyright'>&copy; <a target='_blank' href='"+img['url']+"'>"+img['owner_name']+"</a></div></div></div>"
+  p img['title']
+  p size
+  src = img[size]|| img['medium']
+  p img['url']
+  p img['taken']
+  "<div class='flickr-photo #{klass}'><img title='"+img['title']+"' alt='"+img['title']+"' src='"+src+"'><div class='caption'><div class='photo-date'>"+img['taken']+"</div><div class='copyright'>&copy; <a target='_blank' href='"+img['url']+"'>"+img['owner_name']+"</a></div></div></div>"
 end
 
 module Jekyll
